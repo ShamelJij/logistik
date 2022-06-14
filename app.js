d4c1164f60eb3b9c8bbc98f9214cffd4769c61d2
shamel jij <shamel.jij@gmail.com>
2022-06-14 12:43:32 +0200

    added options and multiple lkws

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 class LKW {
     constructor(name, v, vB, t, k = 0){
         this.name = name;
@@ -9,10 +7,15 @@ class LKW {
         this.tank = t; //gesamt
         this.restTank = t; //rest
     }
-    fahren (k){
+    fahren(k, b){
         k = Math.min(k, (this.reichweite(b) - 0.05).toFixed(1));
         this.kilometer += k;
-        this.restTank -= (k/100) * this.verbrauch ;
+        if(b){
+            this.restTank -= (k/100) * this.verbrauchBeladen;
+        } else {
+            this.restTank -= (k/100)*this.verbrauch;
+        }
+        
     }
     tanken(l){
         l = Math.min(l, (this.tank - this.restTank)); 
@@ -28,23 +31,41 @@ class LKW {
 }
 
 function fahren(){
-    lkw1.fahren(document.getElementById("strecke").value);
+    let id = document.getElementById("lkw").value;
+    lkw[id].fahren(document.getElementById("strecke").value);
+    document.getElementById("beladen").checked;
     info();
 }
 function tanken(){
-    lkw1.tanken(document.getElementById("liter").value);
+    let id = document.getElementById("lkw").value;
+    lkw[id].tanken(document.getElementById("liter").value);
     info();
 }
 
-let lkw1 = new LKW("LKW1", 30, 50, 1000);
-lkw1.fahren(2000);
-lkw1.tanken(400);
-lkw1.fahren(2000);
+lkw = [];
+lkw.push(new LKW("LKW1", 30, 50, 1000));
+lkw.push(new LKW("LKW2", 25, 40, 2500));
+lkw.push(new LKW("LKW3", 35, 65, 1200));
+lkw.push(new LKW("LKW4", 45, 75, 1600));
 info();
 function info(){
-    document.getElementById("info1").innerHTML = "<b class='text-success'>" + lkw1.name + "</b><hr class='hrauto'>" 
-+ "<br>Kilometerstand: " + lkw1.kilometer.toFixed(1) + 
-" km <br>Tankinhalt: " + lkw1.restTank.toFixed(1) + " ℓ/" + lkw1.tank + 
-" ℓ <br> Reichweite: " + (lkw1.reichweite(false).toFixed(1)) + " km"; 
+    document.getElementById("info").innerHTML = "";
+    for(let i = 0; i < lkw.length; i++){
+        document.getElementById("info").innerHTML += "<div class='col-md'><b class='text-warning'>" + lkw[i].name + "</b><hr class='hrauto'>" 
+        + "<br>Kilometerstand: " + lkw[i].kilometer.toFixed(1) + 
+        " km <br>Tankinhalt: " + lkw[i].restTank.toFixed(1) + " ℓ/" + lkw[i].tank + 
+        " ℓ <br> Reichweite: " + (lkw[i].reichweite(document.getElementById("beladen").checked).toFixed(1)) + " km </div><br>"; 
+    }
+
+}
+lkwAuflisten();
+function lkwAuflisten(){
+    let select = document.getElementById("lkw");
+    for(let i = 0; i<lkw.length; i++){
+        let opt = document.createElement("option");
+        opt.value = i;
+        opt.innerHTML = lkw[i].name;
+        select.appendChild(opt);
+    }
 }
 
