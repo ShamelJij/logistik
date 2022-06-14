a66b74b5aa198ac59038c34c0fd67f637a760cc5
shamel jij <shamel.jij@gmail.com>
2022-06-14 10:41:43 +0200

    added form

--- app.js
+++ app.js
@@ -18,13 +18,24 @@ class LKW {
         this.restTank += l;
     }
 }
+function fahren(){
+    lkw1.fahren(document.getElementById("strecke").value);
+    info();
+}
+function tanken(){
+    lkw1.tanken(document.getElementById("liter").value);
+    info();
+}
 
-let lkw1 = new LKW("LKW1", 30, 1000);
+let lkw1 = new LKW("LKW1", 30, 50, 1000);
 lkw1.fahren(2000);
 lkw1.tanken(400);
 lkw1.fahren(2000);
-document.getElementById("info").innerHTML = "<b class='text-success'>" + lkw1.name + "</b>" 
-+ "<br>Kilometerstand: " + lkw1.kilometer + 
+info();
+function info(){
+    document.getElementById("info1").innerHTML = "<b class='text-success'>" + lkw1.name + "</b><hr class='hrauto'>" 
++ "<br>Kilometerstand: " + lkw1.kilometer.toFixed(1) + 
 " km <br>Tankinhalt: " + lkw1.restTank.toFixed(1) + " ℓ/" + lkw1.tank + 
 " ℓ <br> Reichweite: " + (lkw1.restTank/lkw1.verbrauch * 100).toFixed(1) + " km"; 
+}
 
